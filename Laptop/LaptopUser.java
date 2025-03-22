3bb4f70c5fc6f6445ab1c03faac35afa036badf4
Ranggis <ranggis240604@gmail.com>
2025-03-22 10:15:13 +0700

    first commit

---
+++ Laptop/LaptopUser.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Laptop;
+/**
+ *
+ * @author LENOVO
+ */
+// LaptopUser.java
+
+public class LaptopUser {
+    private Laptop laptop;
+    
+    public LaptopUser(Laptop laptop) {
+        this.laptop = laptop;
+    }
+    
+    void turnOnLaptop(){
+        this.laptop.powerOn();
+    }
+    
+    void turnOffLaptop(){
+        this.laptop.powerOff();
+    }
+    
+    void makeLaptopLouder(){
+        this.laptop.volumeUp();
+    }
+    
+    void makeLaptopSilence(){
+        this.laptop.volumeDown();
+    }
+}
